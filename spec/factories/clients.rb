119f256fa6fc4a86cef4b94f8784ea37765474a6
Daniel Sztolcman <daniel.sztolcman@gmail.com>
2021-10-30 13:34:39 +1100

    Add plumbers, addresses and vehicles

--- spec/factories/clients.rb
+++ spec/factories/clients.rb
@@ -18,11 +18,11 @@
 FactoryBot.define do
   factory :client do
     sequence :first_name do |n|
-      "First Name #{n}"
+      "Client First Name #{n}"
     end
 
     sequence :last_name do |n|
-      "Last Name #{n}"
+      "Client Last Name #{n}"
     end
 
     sequence :email do |n|
@@ -30,5 +30,9 @@ FactoryBot.define do
     end
 
     date_of_birth { '17/08/1988' }
+
+    before(:create) do |client|
+      client.address ||= create(:address, addressable: client)
+    end
   end
 end
---
+++ spec/factories/vehicles.rb
@@ -0,0 +1,36 @@
+# == Schema Information
+#
+# Table name: vehicles
+#
+#  id           :bigint           not null, primary key
+#  make         :citext           not null
+#  model        :citext           not null
+#  number_plate :citext           not null
+#  other_make   :citext
+#  year         :integer          not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  plumber_id   :bigint
+#
+# Indexes
+#
+#  index_vehicles_on_number_plate  (number_plate) UNIQUE
+#  index_vehicles_on_plumber_id    (plumber_id)
+#
+FactoryBot.define do
+  factory :vehicle do
+    make { 'toyota' }
+    model { 'Hilux' }
+    sequence :number_plate do |n|
+      n
+    end
+    year { 2020 }
+
+    trait :other do
+      make { 'other' }
+      other_make { 'Delorian' }
+    end
+
+    association :plumber
+  end
+end
