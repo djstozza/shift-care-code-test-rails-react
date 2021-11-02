d56a0a4185a47a3e35e86b7e6910bfa6801c835a
Daniel Sztolcman <daniel.sztolcman@gmail.com>
2021-11-02 18:14:05 +1100

    Add logout

--- app/javascript/packs/navBar/index.tsx
+++ app/javascript/packs/navBar/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import {
   Typography,
   AppBar,
@@ -7,45 +8,72 @@ import {
   Toolbar
 } from '@mui/material'
 import { Link } from 'react-router-dom'
+import { authActions } from 'state/auth'
 
-const NavBar = () => (
-  <Box mb={4} sx={{ flexGrow: 1 }}>
-    <AppBar position='static' color='primary'>
-      <Toolbar>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          Scheduler
-        </Typography>
-        <Button
-          component={Link}
-          to='/clients/new'
-          color='inherit'
-        >
-          New Client
-        </Button>
-        <Button
-          component={Link}
-          to='/plumbers/new'
-          color='inherit'
-        >
-          New Plumber
-        </Button>
-        <Button
-          component={Link}
-          to='/jobs/new'
-          color='inherit'
-        >
-          New Job
-        </Button>
-        <Button
-          component={Link}
-          to='/jobs'
-          color='inherit'
-        >
-          Jobs
-        </Button>
-      </Toolbar>
-    </AppBar>
-  </Box>
-)
+type Props = {
+  logOut: () => void
+}
 
-export default NavBar
+const NavBar = (props) => {
+  const { logOut } = props
+
+  return (
+    <Box mb={4} display='flex' justifyContent='right'>
+      <AppBar position='static' color='primary'>
+        <Toolbar>
+          <Button
+            component={Link}
+            to='/clients/new'
+            color='inherit'
+          >
+            New client
+          </Button>
+          <Button
+            component={Link}
+            to='/plumbers/new'
+            color='inherit'
+          >
+            New plumber
+          </Button>
+          <Button
+            component={Link}
+            to='/jobs/new'
+            color='inherit'
+          >
+            New job
+          </Button>
+          <Button
+            component={Link}
+            to='/jobs'
+            color='inherit'
+          >
+            Jobs
+          </Button>
+          <Button
+            color='inherit'
+            onClick={() => logOut()}
+          >
+            Log out
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  )
+}
+
+
+const mapStateToProps = (state) => {
+  const {
+    auth: { admin }
+  } = state
+
+  return {
+    admin
+  }
+}
+
+const matchDispatchToProps = {
+  logOut: authActions.logOut
+}
+
+export default connect(mapStateToProps, matchDispatchToProps)(NavBar)
